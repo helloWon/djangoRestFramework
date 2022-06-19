8cbcba6ab020fec6b73daffdc9f13feae905c80d
helloWon <leeseongwon13@gmail.com>
2022-06-19 10:44:19 +0900

    viewset으로 API 만들기

--- VueDjAgency/api2/urls.py
+++ VueDjAgency/api2/urls.py
@@ -1,11 +1,12 @@
-# Routers provide an easy way of automatically determining the URL conf.
 from django.urls import include, path
 from rest_framework import routers
 
-from api2.views import UserViewSet
+from api2.views import CommentViewSet, PostViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
+router.register(r"post", PostViewSet)
+router.register(r"comment", CommentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- VueDjAgency/api2/views.py
+++ VueDjAgency/api2/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from blog.models import Comment, Post
 
-from api2.serializers import UserSerializer
+from api2.serializers import CommentSerializer, PostSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
